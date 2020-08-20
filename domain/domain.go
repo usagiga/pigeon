d992bd969fb41c155fcf9ac51481326b48cdb1e6
usagiga <usagigawara@gmail.com>
2020-08-20 16:01:36 +0900

    Define use cases

---
+++ domain/domain.go
@@ -0,0 +1,19 @@
+package domain
+
+import "github.com/usagiga/pigeon/model"
+
+type GitRepositoryUseCase interface {
+	Initialize(repoUrl, articleDir, imageDir string) (repoDir *model.GitRepoDir, err error)
+	CommitAndPush(repoDir *model.GitRepoDir) (err error)
+	Dispose(repoDir *model.GitRepoDir) (err error)
+}
+
+type ArticleBuilderUseCase interface {
+	GetRawArticle(postId int) (rawArticle *model.Article, err error)
+	FormatArticle(rawArticle *model.Article) (formattedArticle *model.Article, err error)
+	Store(repoDir *model.GitRepoDir, article *model.Article) (err error)
+}
+
+type ImageStoreKeeperUseCase interface {
+	Store(repoDir *model.GitRepoDir, url string) (storedUrl string, err error)
+}
