606c3bc513a72030a79998a188f0403629c188a1
usagiga <usagigawara@gmail.com>
2020-08-20 22:18:30 +0900

    Revise config struct tag

--- model/config.go
+++ model/config.go
@@ -1,10 +1,10 @@
 package model
 
 type Config struct {
-	EsaAPIKey string `env:"PIGEON_ESA_KEY"`
-	EsaTeam   string `env:"PIGEON_ESA_TEAM"`
+	EsaAPIKey string `envs:"PIGEON_ESA_KEY"`
+	EsaTeam   string `envs:"PIGEON_ESA_TEAM"`
 
-	DiaryRepoURL string `env:"PIGEON_DIARY_REPO_URL"`
-	ArticleDir   string `env:"PIGEON_PROJECT_ARTICLE_DIR"`
-	ImageDir     string `env:"PIGEON_PROJECT_IMAGE_DIR"`
+	DiaryRepoURL string `envs:"PIGEON_DIARY_REPO_URL"`
+	ArticleDir   string `envs:"PIGEON_PROJECT_ARTICLE_DIR"`
+	ImageDir     string `envs:"PIGEON_PROJECT_IMAGE_DIR"`
 }
