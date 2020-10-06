1729ce66b548c67e1de416f267c9b17e3e361cb7
usagiga <usagigawara@gmail.com>
2020-10-06 15:25:01 +0900

    Implement test for ImageInfra #1

---
+++ infra/image_test.go
@@ -0,0 +1,197 @@
+package infra
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"crypto/tls"
+	"google.golang.org/api/option"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func initTestGCSClient(t *testing.T) (client *storage.Client, err error) {
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+	httpClient := &http.Client{Transport: transCfg}
+
+	clientChan := make(chan *storage.Client)
+	go func() {
+		for i := 0; i <= 5; i++ {
+			c, err := storage.NewClient(context.TODO(), option.WithEndpoint("https://fake-gcs-server:4443/storage/v1/"), option.WithHTTPClient(httpClient))
+			if err != nil {
+				time.Sleep(2 * time.Duration(i) * time.Second)
+				t.Log("Can't initialize GCS client: ", err)
+				continue
+			}
+
+			clientChan <- c
+			return
+		}
+
+		clientChan <- nil
+	}()
+
+	client = <-clientChan
+	if client == nil {
+		t.Fatal("Can't initialize GCS client. Aborting...")
+	}
+
+	return client, nil
+}
+
+func TestImageInfraImpl_Exists(t *testing.T) {
+	// Initialize
+	gcsClient, err := initTestGCSClient(t)
+	if err != nil {
+		t.Fatalf("Can't init test GCS client: %v", err)
+	}
+
+	//noinspection GoUnhandledErrorResult
+	defer gcsClient.Close()
+
+	imageInfra := NewImageInfra("pigeon-test", gcsClient)
+
+	// Declare test cases
+	type Arg struct {
+		fileName string
+	}
+
+	type Result struct {
+		exists bool
+	}
+
+	testCases := []struct {
+		isExpectedError bool
+		arg             Arg
+		result          Result
+	}{
+		// Nominal scenario (existing)
+		{
+			isExpectedError: false,
+			arg: Arg{
+				fileName: "existing.png",
+			},
+			result: Result{
+				exists: true,
+			},
+		},
+		// Nominal scenario (not existing)
+		{
+			isExpectedError: false,
+			arg: Arg{
+				fileName: "not-existing.png",
+			},
+			result: Result{
+				exists: false,
+			},
+		},
+		// On error
+		{
+			isExpectedError: true,
+			arg: Arg{
+				fileName: "error.png",
+			},
+		},
+	}
+
+	// Run test
+	for i, v := range testCases {
+		caseNum := i + 1
+		exists, err := imageInfra.Exists(v.arg.fileName)
+
+		// When raising NOT expected error
+		if err != nil && !v.isExpectedError {
+			t.Errorf("Case %d: This case is not expected to raise error, but error raised; %v", caseNum, err)
+		}
+
+		// When NOT raising expected error
+		if err == nil && v.isExpectedError {
+			t.Errorf("Case %d: This case is expected to raise error, but error didn't raised", caseNum)
+		}
+
+		// When returns NOT expected result
+		if exists != v.result.exists {
+			t.Errorf(
+				"Case %d: Returned 'storedUrl' is not expected.\nExpected:\t%v\nActual:\t%v",
+				caseNum,
+				v.result.exists,
+				exists,
+			)
+		}
+	}
+}
+
+func TestImageInfraImpl_Fetch(t *testing.T) {
+	// Initialize
+	gcsClient, err := initTestGCSClient(t)
+	if err != nil {
+		t.Fatalf("Can't init test GCS client: %v", err)
+	}
+
+	//noinspection GoUnhandledErrorResult
+	defer gcsClient.Close()
+
+	imageInfra := NewImageInfra("pigeon-test", gcsClient)
+
+	// Declare test cases
+	type Arg struct {
+		destPath string
+		srcUrl   string
+	}
+
+	type Result struct {
+		exists bool
+	}
+
+	testCases := []struct {
+		isExpectedError bool
+		arg             Arg
+		result          Result
+	}{
+		// Nominal scenario (existing)
+		{
+			isExpectedError: false,
+			arg: Arg{
+				destPath: "existing.png",
+			},
+			result: Result{
+				exists: true,
+			},
+		},
+		// Nominal scenario (not existing)
+		{
+			isExpectedError: false,
+			arg: Arg{
+				destPath: "not-existing.png",
+			},
+			result: Result{
+				exists: false,
+			},
+		},
+		// On error
+		{
+			isExpectedError: true,
+			arg: Arg{
+				destPath: "error.png",
+			},
+		},
+	}
+
+	// Run test
+	for i, v := range testCases {
+		caseNum := i + 1
+		err := imageInfra.Fetch(v.arg.destPath, v.arg.srcUrl)
+
+		// When raising NOT expected error
+		if err != nil && !v.isExpectedError {
+			t.Errorf("Case %d: This case is not expected to raise error, but error raised; %v", caseNum, err)
+		}
+
+		// When NOT raising expected error
+		if err == nil && v.isExpectedError {
+			t.Errorf("Case %d: This case is expected to raise error, but error didn't raised", caseNum)
+		}
+	}
+}
